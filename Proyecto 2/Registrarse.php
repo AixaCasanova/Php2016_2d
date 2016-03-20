bb7bfc00258a89ba13e6b15d667e2da615bd70b1
Aixa <aixacasanova@gmail.com>
2016-03-20 16:27:50 -0300

    Commit 5 Proyecto 2
    Se agrega un link para registrarse

---
+++ Proyecto 2/Registrarse.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<span>Nombre:</span> 
+      
+<input id="name" type="text" name="Nombre" placeholder="Nombre completo" />
+</br></br>
+<span>Direccion:</span> 
+        
+<input id="name" type="text" name="Direccion" placeholder="Direccion" />
+  </br></br>
+<span>Telefono:</span> 
+        
+<input id="name" type="number" name="Telefono" placeholder="Telefono" />
+   </br></br>
+<span>Email:</span> 
+        
+<input id="name" type="email" name="Email" placeholder="Ejemplo@mail.com" />
+ 	 </br></br>
+ 	
+<input type="button" name="Registrarme" value="Registrarme!"/>
+ 
+</body>
+</html>
