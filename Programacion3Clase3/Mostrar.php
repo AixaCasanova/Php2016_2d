2b630203f36bd791ac15541a0f7f7f88dff14da6
AixaCasanova <aixacasanova@gmail.com>
2016-03-28 20:14:41 -0300

    Clase 3 : parte 2

--- Programacion3Clase3/Mostrar.php
+++ Programacion3Clase3/Mostrar.php
@@ -2,8 +2,19 @@
 <h3> 
 <?php
 echo "Recibidos:<br>";
-var_dump($_POST);
-echo "<br>";
-var_dump($_GET);
+//var_dump($_POST);
+//var_dump($_GET);
+
+$nombre= $_POST['usuario'];
+$clave= $_POST['clave'];
+echo "Nombre: ".$nombre;
+echo "<br>Clave: ".$clave;
+
+if ($nombre == "Admin" && $clave =="1234") {
+	//include_once("Bienvenido.html");
+	include("Bienvenido.html");
+	//require("Bienvenido.html");
+}
+
 ?>
 </h3> 
